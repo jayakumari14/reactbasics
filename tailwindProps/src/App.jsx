659d70fe7052c93afb2cf191553237ab6d5ce792
jayak <kumarijaya127@gmail.com>
2024-05-08 15:54:53 +0530

    add props

--- tailwindProps/src/App.jsx
+++ tailwindProps/src/App.jsx
@@ -1,29 +1,23 @@
 import "./App.css";
-
+import Card from "./components/Card";
 function App() {
+  // let users = {
+  //   name: "deena",
+  //   age: 20,
+  // };
+
+  // let arr = [1, 2, 3, 4, 5];
+  let userImg =
+    "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  let userImagge =
+    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
   return (
     <>
       <h1 className="text-red-500 bg-green-300 p-5 text-4xl rounded mb-5">
         welcome to vite and tailwind{" "}
       </h1>
-      <div className="relative h-[400px] w-[300px] rounded-md">
-        <img
-          src="https://images.unsplash.com/photo-1546961329-78bef0414d7c?ixlib=rb-4.0.3&amp;ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHVzZXJ8ZW58MHx8MHx8&amp;auto=format&amp;fit=crop&amp;w=800&amp;q=60"
-          alt="AirMax Pro"
-          className="z-0 h-full w-full rounded-md object-cover"
-        />
-        <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-        <div className="absolute bottom-4 left-4 text-left">
-          <h1 className="text-lg font-semibold text-white">Delba</h1>
-          <p className="mt-2 text-sm text-gray-300">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
-            debitis?
-          </p>
-          <button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-            View Profile →
-          </button>
-        </div>
-      </div>
+      <Card username="dania" userImg={userImg} />
+      <Card username="james" btn="visit profile" userImg={userImagge} />
     </>
   );
 }
