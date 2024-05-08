c6790cdfea7f6b008a2dc1405d4a7f0b66e96f31
jayak <kumarijaya127@gmail.com>
2024-05-08 13:10:56 +0530

    add counter

--- counterApp/src/App.jsx
+++ counterApp/src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
+
 function App() {
-  let counter = 5;
+  const [counter, setcounter] = useState(15);
 
   const addValue = () => {
-    console.log("clicked", Math.random());
+    setcounter(counter + 1);
+  };
+  const removeValue = () => {
+    setcounter(counter - 1);
   };
   return (
     <>
       <h1>Counter App</h1>
       <h2>Counter Value: {counter}</h2>
-      <button onClick={addValue}>Add Value</button>
-      <button>Remove Value</button>
+      <button onClick={addValue}>Add Value{counter}</button>
+      <button onClick={removeValue}>Remove Value{counter}</button>
     </>
   );
 }
