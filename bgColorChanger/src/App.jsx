43238824d15f3fab6ecfac5d1b91367f992c8ca0
jayak <kumarijaya127@gmail.com>
2024-05-08 16:27:47 +0530

    add bgColor changer

---
+++ bgColorChanger/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-center text-4xl tracking-tighter mt-5">
+        BackGround Color Changer
+      </h1>
+    </>
+  );
+}
+
+export default App;
