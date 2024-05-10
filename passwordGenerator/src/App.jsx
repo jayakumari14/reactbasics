e35e67a42151d3b8c7ea100cfca5d45772bd0ca5
jayak <kumarijaya127@gmail.com>
2024-05-10 19:28:46 +0530

    add input buttons UI

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,5 +1,4 @@
 import { useState, useCallback } from "react";
-import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
@@ -9,12 +8,59 @@ function App() {
 
   const passwordGenerator = useCallback(() => {
     let pass = "";
-    let str = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*-_+={}[]~'";
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
   }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
-      <h1 className="text-4xl text-white text-center">Password Generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+        <h1 className="text-4xl text-white text-center">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden  mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3 rounded-md mt-5 mb-3"
+            placeholder="password"
+            readOnly
+          />
+          <button className="outline-none  text-blue-500 px-3  ">copy</button>
+        </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length: {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput">Numbers</label>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
