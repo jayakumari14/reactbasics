61d4c81f9e0020edd4a874a20c7d58350b3c1ce3
jayak <kumarijaya127@gmail.com>
2024-05-10 17:58:20 +0530

    add usecallback

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
-  const [char, setChar] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
   const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "";
+  }, [length, numberAllowed, charAllowed, setPassword]);
 
   return (
     <>
